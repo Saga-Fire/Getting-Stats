7fca7f8ac45d8ff59714a19d32fb4f88847e3489
Saga-Fire <75118944+Saga-Fire@users.noreply.github.com>
2020-11-27 14:03:41 +0100

    first commit

--- Statistique_Mikhail.js
+++ Statistique_Mikhail.js
@@ -4,22 +4,22 @@ let inputInt = null;
 let totalInput = 0;
 let minInput = null;
 let maxInput = null;
-let sumInput = 0;
+let sumInput = null;
 let meanInput = null;
 let i = null;
 let number = null;
 let message = 'Vous n\'avez saisi aucun nombre';
 
-input = prompt('Veuillez saisir un nombre\n pour terminer la saisie')
+input = prompt('Veuillez saisir un nombre\n0 pour terminer la saisie')
 inputInt = parseInt(input)
 
 if (inputInt != 0){
     while (inputInt != 0){
         if (inputInt){
-            console.log('Vous venez d\'entrer le nombre : ' + inputInt)
+            console.log(`Vous venez d'entrer le nombre : ${inputInt}`)
             listInput.push(inputInt)
             sumInput = sumInput + inputInt
-            input = prompt('Veuillez saisir un nombre\n0 pout terminer la saisie')
+            input = prompt('Veuillez saisir un nombre\n0 pour terminer la saisie')
             inputInt = parseInt(input)
         } else {
             input = prompt('Ceci n\'est pas un nombre\nVeuillez saisir un nombre\n0 pour terminer la saisie')
@@ -30,13 +30,11 @@ if (inputInt != 0){
         totalInput = listInput.length
         console.log(listInput)
         minInput = listInput[0]
+        maxInput = listInput[0]
         for ( i = 1 ; i <= totalInput ; i++){
             if (minInput > listInput[i]){
                 minInput = listInput[i]
             }
-        }
-        maxInput = listInput[0]
-        for ( i = 1 ; i <= totalInput ; i++){
             if (maxInput < listInput[i]){
                 maxInput = listInput[i]
             }
@@ -47,7 +45,7 @@ if (inputInt != 0){
         } else {
             number = "nombres"
         }
-        message = 'Vous avez saisi ' + totalInput + ' ' + number + '\nCompris entre ' + minInput + ' et ' + maxInput + '\nPour une somme de ' + sumInput + '\nEt une moyenne de ' + meanInput
+        message = `Vous avez saisi ${totalInput} ${number}\nCompris entre ${minInput} et ${maxInput}\nPour une somme de ${sumInput}\nEt une moyenne de ${meanInput}`
         console.log(message)
         alert(message)
     } else {
